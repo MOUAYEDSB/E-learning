c8ca397391d4fd5ac0a987d69ff47bae503c94e1
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-09-17 13:12:09 +0100

    fix

--- backend/models/modeleUser.js
+++ backend/models/modeleUser.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 
 
 const options = { discriminatorKey: 'role', collection: 'users' };
@@ -18,25 +17,6 @@ const userSchema = new Schema({
   age: Number,
   role: { type: String, enum: ['formateur', 'enfant', 'parent'], required: true }
 }, options);
-
-// Pre-save hook to hash passwords
-userSchema.pre('save', async function (next) {
-  if (this.isModified('motdepasse')) {
-    this.motdepasse = await bcrypt.hash(this.motdepasse, 10);
-  }
-  next();
-});
-
-// Method to compare passwords
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.motdepasse);
-};
-
-// Method to generate JWT token
-userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id, role: this.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  return token;
-};
 
 
 const User = mongoose.model('User', userSchema);
